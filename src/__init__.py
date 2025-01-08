084890c9c977124fcb2f207befd596bc3669d355
Marceau-h <git@marceau-h.fr>
2025-01-08 14:29:52 +0100

    Fix relative imports in src module

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
-from Language import Language, read_data
-from model import S2SBiLSTM
-from train import auto_train, train
-from eval import core_eval, random_predict, evaluate, align_words
+from .Language import Language, read_data
+from .model import S2SBiLSTM
+from .train import auto_train, train
+from .eval import core_eval, random_predict, evaluate, align_words
 
 __all__ = [
     "Language",
