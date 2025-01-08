18c933cfdccfee9b391e781e8c2c64d4682aef56
Marceau-h <git@marceau-h.fr>
2025-01-08 14:10:18 +0100

    Refactor evaluation functions & replaced broken accuracy \w exact match

--- src/eval.py
+++ src/eval.py
@@ -1,18 +1,20 @@
 import re
-from random import choice
+from random import sample
+from statistics import mean
 
+from jiwer import process_words, visualize_alignment
 from torch import device as torch_device
 from torch.cuda import is_available
-from jiwer import process_words, visualize_alignment
-from tqdm.auto import trange
+from tqdm.auto import trange, tqdm
 
 multi_stars = re.compile(r"\*{2,}")
+
 
 def align_words(ref, hyp):
     if ref == hyp:
         return None, 0
 
-    computed = process_words([ref], [hyp], lambda x : x, lambda x : x)
+    computed = process_words([ref], [hyp], lambda x: x, lambda x: x)
     alignement = visualize_alignment(computed, show_measures=False)
     _, ref, hyp, *_ = multi_stars.sub("@", alignement).split("\n")
     alignement = f"{ref[5:]}\n{hyp[5:]}".replace(" ", " | ")
@@ -20,52 +22,13 @@ def align_words(ref, hyp):
     return alignement, computed.wer
 
 
-def random_predict(X_test, y_test, lang_input, lang_output, model, device=None, print_output=True, nb_predictions=10):
-    device = device or torch_device("cuda" if is_available() else "cpu")
+def core_eval(X_test, y_test, lang_input, lang_output, model, nb_predictions=None, device=None):
+    if nb_predictions is None:
+        pbar = trange(len(X_test), desc="Evaluating", unit="sentence")
+    else:
+        pbar = tqdm(sample(range(len(X_test)), nb_predictions), desc="Evaluating", unit="sentence")
 
     res = []
-    for _ in range(nb_predictions):
-        random_idx = choice(range(len(X_test)))
-        random_sentence = X_test[random_idx]
-        random_target = y_test[random_idx]
-
-        input_sentence_lst = (
-            [lang_input.index2token[token] for token in random_sentence if token != lang_input.PAD_ID])
-        target_output_lst = ([lang_output.index2token[token] for token in random_target if token != lang_output.PAD_ID])
-
-        predicted_output_lst = model.predict(random_sentence, max_len=lang_output.n_tokens + 1, device=device, lang_output=lang_output)
-
-        accuracy_score = sum([1 for i, j in zip(target_output_lst, predicted_output_lst) if i == j]) / len(
-            target_output_lst)
-
-        aligned, wer = align_words(target_output_lst, predicted_output_lst)
-
-
-        if print_output:
-            print(f"""
-Lengths (\\wo EOS) - Input: {len(input_sentence_lst) - 3}, Target: {len(target_output_lst) - 1}, Predicted: {len(predicted_output_lst) - 1}
-
-Input sentence: {"".join(input_sentence_lst)}
-Target output: {" | ".join(target_output_lst)}
-Predicted output: {" | ".join(predicted_output_lst)}
-
-Alignment:\n{aligned}
-
-Accuracy: {accuracy_score:.2f}
-WER: {wer:.2f}
-""")
-
-        res.append((input_sentence_lst, target_output_lst, predicted_output_lst, aligned, accuracy_score, wer))
-
-    return res
-
-
-def evaluate(X_test, y_test, lang_input, lang_output, model, device=None):
-    device = device or torch_device("cuda" if is_available() else "cpu")
-
-    res = []
-    res_for_save = []
-    pbar = trange(len(X_test), desc="Evaluating", unit="sentence")
     for i in pbar:
         input_sentence = X_test[i]
         target_output = y_test[i]
@@ -74,34 +37,67 @@ def evaluate(X_test, y_test, lang_input, lang_output, model, device=None):
             [lang_input.index2token[token] for token in input_sentence if token != lang_input.PAD_ID])
         target_output_lst = ([lang_output.index2token[token] for token in target_output if token != lang_output.PAD_ID])
 
-        predicted_output_lst = model.predict(input_sentence, max_len=lang_output.n_tokens + 1, device=device, lang_output=lang_output)
+        predicted_output_lst = model.predict(input_sentence, max_len=lang_output.n_tokens + 1, device=device,
+                                             lang_output=lang_output)
 
-        accuracy_score = sum([1 for i, j in zip(target_output_lst, predicted_output_lst) if i == j]) / len(
-            target_output_lst)
+        exact_match = target_output_lst == predicted_output_lst
 
         aligned, wer = align_words(target_output_lst, predicted_output_lst)
 
-        res.append((input_sentence_lst, target_output_lst, predicted_output_lst, aligned, accuracy_score, wer))
-        res_for_save.append({
-            "input": "".join(input_sentence_lst),
-            "target": " | ".join(target_output_lst),
-            "predicted": " | ".join(predicted_output_lst),
-            "alignment": aligned,
-            "target_length": len(target_output_lst) ,
-            "predicted_length": len(predicted_output_lst),
-            "accuracy": accuracy_score,
-            "wer": wer
+        res.append((input_sentence_lst, target_output_lst, predicted_output_lst, aligned, exact_match, wer))
+
+    return res
+
+
+def random_predict(X_test, y_test, lang_input, lang_output, model, device=None, print_output=True, nb_predictions=10):
+    device = device or torch_device("cuda" if is_available() else "cpu")
+
+    res = core_eval(X_test, y_test, lang_input, lang_output, model, nb_predictions, device)
+
+    if print_output:
+        for input_sentence_lst, target_output_lst, predicted_output_lst, aligned, exact_match, wer in res:
+            print(f"""
+Lengths (\\wo EOS) - Input: {len(input_sentence_lst) - 6}, Target: {len(target_output_lst) - 2}, Predicted: {len(predicted_output_lst) - 2}
+
+Input sentence: {"".join(input_sentence_lst)}
+Target output: {" | ".join(target_output_lst)}
+Predicted output: {" | ".join(predicted_output_lst)}
+
+Alignment:\n{aligned}
+
+Exact match: {exact_match}
+WER: {wer:.2f}
+""")
+
+    return res
+
+
+def evaluate(X_test, y_test, lang_input, lang_output, model, device=None):
+    device = device or torch_device("cuda" if is_available() else "cpu")
+
+    res = core_eval(X_test, y_test, lang_input, lang_output, model, None, device)
+    len_res = len(res)
+
+    exact_match = mean(r[-2] for r in res)
+
+    print(f"Exact match ratio: {exact_match:.2f}")
+
+    wer_score = mean(r[-1] for r in res)
+
+    print(f"Mean WER: {wer_score:.2f}")
+
+    res_for_save = [
+        {
+            "input": "".join(r[0]),
+            "target": " | ".join(r[1]),
+            "predicted": " | ".join(r[2]),
+            "alignment": r[3],
+            "target_length": len(r[1]),
+            "predicted_length": len(r[2]),
+            "exact_match": r[4],
+            "wer": r[5]
         }
-        )
+        for r in res
+    ]
 
-    accuracy_scores = [r[-2] for r in res]
-    accuracy = sum(accuracy_scores) / len(accuracy_scores)
-
-    print(f"Accuracy: {accuracy:.2f}")
-
-    wer_scores = [r[-1] for r in res]
-    wer_score = sum(wer_scores) / len(wer_scores)
-
-    print(f"WER: {wer_score:.2f}")
-
-    return res, accuracy, wer_score, res_for_save
+    return res, exact_match, wer_score, res_for_save
