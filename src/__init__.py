476bead7f9a04c46e24d70c8721cd975c9384003
Marceau-h <git@marceau-h.fr>
2025-01-08 14:12:33 +0100

    Add `__init__.py` file to define module exports

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+from Language import Language, read_data
+from model import S2SBiLSTM
+from train import auto_train, train
+from eval import core_eval, random_predict, evaluate, align_words
+
+__all__ = [
+    "Language",
+    "read_data",
+    "S2SBiLSTM",
+    "auto_train",
+    "train",
+    "core_eval",
+    "random_predict",
+    "evaluate",
+    "align_words"
+]
